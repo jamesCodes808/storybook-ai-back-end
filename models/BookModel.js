2c101f3395b38756ac844b7d6fb41addfe5478eb
ekalbers <ekalbers22@gmail.com>
2023-03-07 17:55:58 -0600

    added auth0 functionality for get and delete

--- models/BookModel.js
+++ models/BookModel.js
@@ -8,6 +8,10 @@ const bookSchema = new mongoose.Schema({
     story: {
         type: String,
         required: true
+    },
+    email: {
+        type: String,
+        required: true
     }
 });
 
