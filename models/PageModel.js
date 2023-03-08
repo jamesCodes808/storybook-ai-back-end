292489d6fc849265174b10aa43758a1c701b9760
ekalbers <ekalbers22@gmail.com>
2023-03-08 17:31:03 -0600

    working towards implementing pages

---
+++ models/PageModel.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+
+const pageSchema = new mongoose.Schema({
+    picture: String,
+    text: String
+});
+
+module.exports = mongoose.model('Page', pageSchema);
